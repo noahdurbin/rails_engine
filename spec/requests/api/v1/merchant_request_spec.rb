451db4dd2ba61b568a02e27f3f925de1702a5238
Noah Durbin <13364668+noahdurbin@users.noreply.github.com>
2024-07-08 17:52:42 -0600

    test: merchant index spec test

---
+++ spec/requests/api/v1/merchant_request_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+describe 'merchants API' do
+  it 'sends a list of merchants' do
+    create_list(:merchant, 3)
+
+    get '/api/v1/merchants'
+
+    expect(response).to be_successful
+  end
+end
