bcf4559096f411696ad18db84b5b06eb43c3546b
Rodrigo Chavez <Rodrigo103004@gmail.com>
2024-07-11 14:07:09 -0600

    add merchants search route

--- config/routes.rb
+++ config/routes.rb
@@ -7,10 +7,11 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
+      get '/merchants/find', to: 'merchants#search'
       resources :merchants, only: [:index, :show] do
         resources :items, only: [:index]
       end
-
+      
       resources :items do
         get '/merchant', to: 'items#merchant'
       end
