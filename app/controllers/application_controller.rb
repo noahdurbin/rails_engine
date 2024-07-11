b14d00dee44abdaaf5ad7175015edf58c98fc145
Noah Durbin <13364668+noahdurbin@users.noreply.github.com>
2024-07-11 10:43:33 -0600

    feat: refactoring to satisfy postman tests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,19 +3,15 @@ class ApplicationController < ActionController::API
   rescue_from ActiveRecord::RecordInvalid, with: :invalid_parameters_response
 
 
-  private 
+  private
+
   def not_found_response(error)
     render json: ErrorSerializer.new(ErrorMessage.new(error.message, 404)).serialize_json, status: 404
   end
 
   def invalid_parameters_response(error)
     render json: ErrorSerializer.new(
-      ErrorMessage.new(error.message, 422)
-    ).serialize_json, status: 422
+      ErrorMessage.new(error.message, 400)
+    ).serialize_json, status: 400
   end
 end
-
-
-
-
-
