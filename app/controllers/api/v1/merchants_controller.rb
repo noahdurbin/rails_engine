638b72cdc467a37fe3d776b1425005d62a41b66f
Noah Durbin <13364668+noahdurbin@users.noreply.github.com>
2024-07-11 19:13:36 -0600

    feat: search functionality

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -8,16 +8,24 @@ class Api::V1::MerchantsController < ApplicationController
   end
 
   def search
-    if params[:name].present?
-      merchant = Merchant.search(params[:name])
-
-      if merchant.nil?
-        render json: ErrorSerializer.new(ErrorMessage.new("Record Not Found", 404)).serialize_json, status: 404
+    begin
+      merchant = Merchant.search(params)
+      if merchant
+        render json: MerchantSerializer.new(merchant)
       else
-       render json: MerchantSerializer.new(merchant), status: 200
+        render json: { data: {} }
       end
-    else
-      render json: ErrorSerializer.new(ErrorMessage.new("Name Must Be Filled In", 400)).serialize_json, status: 400
+    rescue ArgumentError => e
+      render json: ErrorSerializer.new(ErrorMessage.new(e.message, 400)).serialize_json, status: :bad_request
+    end
+  end
+
+  def search_all
+    begin
+      merchants = Merchant.search_all(params)
+      render json: MerchantSerializer.new(merchants)
+    rescue ArgumentError => e
+      render json: ErrorSerializer.new(ErrorMessage.new(e.message, 400)).serialize_json, status: :bad_request
     end
   end
 end
--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,12 +1,49 @@
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
 
-  def self.search(search_params)
-    where("name ILIKE ?", "%#{search_params}%").first
+  def self.search(params)
+    if params[:name].present? && (params[:max_price].present? || params[:min_price].present?)
+      raise ArgumentError, 'Cannot search by both name and price'
+    elsif params[:name].present?
+      search_by_name(params[:name]).first
+    elsif params[:max_price].present? || params[:min_price].present?
+      search_by_price(params).first
+    else
+      raise ArgumentError, 'No valid search parameters provided'
+    end
   end
 
-  def self.search_all(search_params)
-    where("name ILIKE ?", "%#{search_params}%")
+  def self.search_all(params)
+    if params[:name].present? && (params[:max_price].present? || params[:min_price].present?)
+      raise ArgumentError, 'Cannot search by both name and price'
+    elsif params[:name].present?
+      search_by_name(params[:name])
+    elsif params[:max_price].present? || params[:min_price].present?
+      search_by_price(params)
+    else
+      raise ArgumentError, 'No valid search parameters provided'
+    end
   end
 
+  private
+
+  def self.search_by_name(name)
+    where("name ILIKE ?", "%#{name}%").order(Arel.sql("LOWER(name)"))
+  end
+
+  def self.search_by_price(params)
+    min_price = params[:min_price].to_f
+    max_price = params[:max_price].to_f
+
+    if min_price.negative? || max_price.negative?
+      raise ArgumentError, "Prices must be greater than or equal to 0"
+    elsif max_price.positive? && min_price >= max_price
+      raise ArgumentError, "Max price cannot be lower than or equal to min price"
+    end
+
+    items = Item.all
+    items = items.where("unit_price >= ?", min_price) if params[:min_price].present?
+    items = items.where("unit_price <= ?", max_price) if params[:max_price].present?
+    items.order(Arel.sql("LOWER(name)"))
+  end
 end
